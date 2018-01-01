cfde12aefd3292b5a1b60e8f06fc72bc9e7a3afc
Aravind <aravind.1@outlook.com>
2018-01-01 20:39:43 +0530

    compile script changed. source_file folder renamed.

--- III/02/compile_script.sh
+++ III/02/compile_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-javac Source/*.java -d ./
-javac BankDemo.java -cp ./
-echo "Compiled"
-java BankDemo 
+javac ./source_files/Transaction.java -d ./
+javac ./source/InterestRate.java -d ./
+javac ./source/Account.java -d ./
+javac ./source/SBAccount.java -d ./
+javac ./source/FDAccount.java -d ./
+javac ./source/Customer.java -d ./
+javac ./BankDemo.java -d ./
+echo "compiled all source files ..."
+echo "execution of program begins ..."
+java BankDemo
