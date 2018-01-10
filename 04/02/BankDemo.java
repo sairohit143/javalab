cd8467102b82d4fd38a631a063a18c542459f3ee
Aravind <aravind.1@outlook.com>
2018-01-10 10:41:12 +0530

    nonsensical commit.

--- 04/02/BankDemo.java
+++
@@ -1,7 +0,0 @@
-import ;
-
-public class BankDemo{
-  public static void main(String[] args){
-
-  }
-}
--- 04/02/source_files/BankDemo.java
+++ 04/02/source_files/BankDemo.java
@@ -1,11 +1,7 @@
-import pkbanking.pkcustomer.*;
+import ;
 
 public class BankDemo{
   public static void main(String[] args){
-    Customer c = new Customer(1001, "Aravind", "none");
-    c.createAccount(1);
-    c.createAccount(2);
-    c.transaction(1);
-    c.transaction(2);
+
   }
 }
