2e004fe5b0e9808df0f3f5b2bd7149b941bd27e3
Aravind M <aravind.1@outlook.com>
2018-01-24 09:55:51 +0530

    re-organisation

--- 03/02/compile_script.sh
+++ 03/02/compile_script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #compilation of auxillary source files from the ./source_files subdirectory.
-javac ./source_files/Transaction.java -d ./
-javac ./source_files/InterestRate.java -d ./
-javac ./source_files/Account.java -d ./
-javac ./source_files/SBAccount.java -d ./
-javac ./source_files/FDAccount.java -d ./
-javac ./source_files/Customer.java -d ./
+javac ./source_files/Transaction.java -d ./class_files
+javac ./source_files/InterestRate.java -d ./class_files
+javac ./source_files/Account.java -d ./class_files
+javac ./source_files/SBAccount.java -d ./class_files
+javac ./source_files/FDAccount.java -d ./class_files
+javac ./source_files/Customer.java -d ./class_files
 
 #compilation of the BankDemo source file that contains the main method.
-javac ./source_files/BankDemo.java -d ./
+javac ./source_files/BankDemo.java -d ./class_files
 
 #notifying the end user that the compilation is over.
 echo "compilation of source files completed ..."
+
+cd ./class_files
 
 #notifying the end user that the program is about to begin.
 echo "execution of program begins ..."
