864640b9429b394c67175b3b581c72b5257e7530
Aravind M <aravind.1@outlook.com>
2018-01-31 08:06:59 +0530

    class_files subdirectory and changed compile script for ./04/02

--- 04/02/compile_script.sh
+++ 04/02/compile_script.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 #compiling other auxillary source_files
-javac ./source_files/ -d ./
-
+javac ./source_files/InterestRate.java -d ./class_files/
+javac ./source_files/Transaction.java -d ./class_files/
+javac ./source_files/InsufficientFundsException.java -d ./class_files/
+javac ./source_files/Account.java -d ./class_files/
+javac ./source_files/SBAccount.java -d ./class_files/
+javac ./source_files/FDAccount.java -d ./class_files/
+javac ./source_files/Customer.java -d ./class_files/
 
 #compiling the main File
-javac ./source_files/BankDemo.java -d ./
+javac ./source_files/BankDemo.java -d ./class_files/
 
-
+#moving into ./class_files/ sub directory.
+cd ./class_files/
 
 #executing the Program
 java BankDemo
